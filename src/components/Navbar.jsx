b7fe0c894018681d84abac9e829e9d89eeb2b8b4
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2025-03-09 23:20:06 +0530

    Cart updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -152,6 +152,25 @@ const Navbar = () => {
     }
   };
 
+  const [cartCount, setCartCount] = useState(0);
+  useEffect(() => {
+    const userLoggedIn = localStorage.getItem("userLoggedIn");
+
+    if (!userLoggedIn) {
+        const selectedCartProducts = JSON.parse(localStorage.getItem("SelectedCartProducts")) || [];
+        setCartCount(selectedCartProducts.length);
+    } else {
+        setCartCount(0);
+    }
+    const handleStorageChange = () => {
+        const updatedCart = JSON.parse(localStorage.getItem("SelectedCartProducts")) || [];
+        setCartCount(updatedCart.length);
+    };
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+}, []);
+
+
   return (
     <div className="relative text-white z-20 text-lg bg-white">
       <div className="w-[90%] m-auto max-w-[1224px] flex items-center justify-between sticky top-0">
@@ -343,12 +362,17 @@ const Navbar = () => {
             className="md:hidden cursor-pointer"
           >
             <NavLink to="/cart" className="text-black px-8 relative">
-              <span>
+            <span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                 </svg>
-              </span>
-            </NavLink>
+            </span>
+            {cartCount > 0 && (
+                <span className="absolute top-4 right-0 bg-pink-500 text-white text-[8px] rounded-full w-4 h-4 flex items-center justify-center">
+                    {cartCount}
+                </span>
+            )}
+        </NavLink>
           </div>
           <div className="cursor-pointer text-yellow-600" onClick={() => setIsSearchOpen(true)}>
             <svg
